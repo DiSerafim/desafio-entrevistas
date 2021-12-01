0f1112ca5ee0a6a69a1ab41272eacdd2ebed7ce9
Diego Serafim <diegoserafim1@gmail.com>
2021-12-01 12:23:22 -0300

    Update desafioFord.js

--- desafioFord.js
+++ desafioFord.js
@@ -1,1 +1,24 @@
-Desafios Propostos nas entrevistas
+// Desafios Propostos nas entrevistas
+
+// Desafio Ford.
+
+// let s1 = vinNumber.length < 3;
+// let s2 = vinNumber.length > 2 && vinNumber.length < 9;
+// let s3 = vinNumber.length < 9;
+
+// console.log(s1);
+
+const crypto = require('crypto');
+
+let vinNumber = '1HGCM82633A004352'; // 17
+let vim = vinNumber;
+let encrypt = crypto.createCipheriv(vim);
+let crypted = encrypt.update(vim, 'utf8');
+console.log(crypted);
+
+// laranja
+
+/*
+obs:
+não sei se aparece para vocês, mas estou postando o desafio fora do horario limite (meu pc travou e precisei reiniciar)
+*/ 
