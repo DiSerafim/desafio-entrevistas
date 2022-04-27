81cf1feb6174e40ef5e23ae0032d930f6356b21d
Diego Serafim <diegoserafim1@gmail.com>
2022-04-27 15:55:16 -0300

    criado css do projeto

--- logicaEsql/frontend/src/component/Diagrama.js
+++ logicaEsql/frontend/src/component/Diagrama.js
@@ -1,29 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './Diagrama.css';
+import diagram from '../images/diagram.png';
+import axios from 'axios';
 
 const Diagrama = () => {
+    const [birds, setGetBirds] = useState([]);
+
+    useEffect(() => {
+        async function fetchBirds() {
+            const URL = `https://audubon-society-api.herokuapp.com/birds`
+            try {
+               const res = await axios.get(URL)
+               console.log(res.data, 'Aqui')
+               setGetBirds(res.data)
+            } catch (error) {
+               console.log(error)
+            }
+        }
+        fetchBirds();
+    }, []);
     
+    if (!birds) return <h3>Loading...</h3>
 
     return (
         <div className="di">
             <div className="question">
-                Considerando o diagrama de classes abaixo, temos um objeto “List&lt;Casa&gt; casas” que pode conter N elementos. Exiba para o usuário o “Endereço”, “Metragem” e a quantidade de “Comodos” apenas das casas que possuam pelo menos 1 comodo com numero par de janelas.
+                <p>3 - Considerando o diagrama de classes abaixo, temos um objeto “List&lt;Casa&gt; casas” que pode conter N elementos. Exiba para o usuário o “Endereço”, “Metragem” e a quantidade de “Comodos” apenas das casas que possuam pelo menos 1 comodo com numero par de janelas.</p>
             </div>
             <div className="rules">
                 <ul>
                     Regras:
                     <li>
-                        +Endereço : string
-                        +Cor : string
-                        +Metragem : int
-                        +Comodos : List&lt;Comodo&gt;
-                        Casa
-                    </li>
-                    <li>
-                        +Nome : string
-                        +Janelas : int
-                        +Portas : int
-                        Comodo
+                        <img src={diagram} alt='diagrama'></img>
                     </li>
                     Considerações:
                     <li>
@@ -36,6 +44,19 @@ const Diagrama = () => {
             </div>
             <div className="result">
                 <h3>Resultado</h3>
+                <label>
+                    <select className="select">
+                        <option>Escolha sua casa</option>
+                        {birds.map((item, index) => 
+                            <option key={index}>{item.name}</option>
+                            // <img src={item.image} alt={index}></img>
+                        )}
+                    </select>
+                </label><br></br>
+                { }
+                <div>
+                    <p>Pendente!!</p>
+                </div>
             </div>
             <hr></hr><br></br>
         </div>
--- logicaEsql/frontend/src/component/Header.js
+++ logicaEsql/frontend/src/component/Header.js
@@ -6,7 +6,6 @@ const Header = () => {
         <header className="App-header">
             <img src='https://www.geniol.com.br/media/cache/c2/71/c271bf717d9ff55968a2e57c79c56f47.png' className="App-logo" alt="logo" />
             <p>Teste de Lógica</p>
-            <hr></hr><br></br>
         </header>
     );
 }
